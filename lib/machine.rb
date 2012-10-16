3c9fb4af836262289bab7d37dfdf1823d41634a2
chatgris <jboyer@af83.com>
2012-10-16 11:50:13 +0200

    Add darwin infos.
    Thanks to @anicet
    
    But incomplete on memory.
    
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/machine.rb
+++ lib/machine.rb
@@ -5,5 +5,7 @@ require_relative 'machine/default/info'
 case RbConfig::CONFIG['host_os']
 when /linux/
   require_relative 'machine/linux/info'
+when /darwin/
+  require_relative 'machine/darwin/info'
 end
 require_relative 'machine/process_status'
---
+++ lib/machine/darwin/info.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+module Machine
+  class Info
+    class Cpu < CpuDefault
+      class << self
+        def cores_size
+          `sysctl -n hw.ncpu`.to_i
+        end
+      end
+    end
+
+    class Memory
+      class << self
+        def mem_total
+          `sysctl -n hw.memsize`.to_i
+        end
+      end
+    end
+  end
+end
+
